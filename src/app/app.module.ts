eb40a4667ddc3044cb9fe0b0deecb32dbe7a6417
xxCr1txx101 <xxcr1txx101@gmail.com>
2021-10-06 20:54:28 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RandomNumComponent } from './random-num/random-num.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomNumComponent
   ],
   imports: [
     BrowserModule
